ad4e8fc258c4df6f1bb8a76e62acd45cba8f98b1
zachmshort <zach.short@fantomworks.com>
2025-05-28 10:55:20 -0400

    2293 leetcode

---
+++ content/leetcode/2293-min-max-game.tsx
@@ -0,0 +1,53 @@
+const goSolution = `func minMaxGame(nums[]int) int {
+  n:= len(nums)
+
+  if n == 1 {
+    return nums[0]
+  }
+
+  var newNums []int
+
+  for i := 0; i < n / 2; i++ {
+    if i % 2 == 0 {
+      newNums = append(newNums, min(nums[2 * i], nums[2 * i + 1]))
+    } else {
+      newNums = append(newNums, max(nums[2 * i], nums[2 * i + 1]))
+    }
+  }
+
+  return minMaxGame(newNums)
+}
+
+func min(num1, num2 int) int {
+  if (num1 <= num2) {
+    return num1
+  }
+
+  return num2
+}
+
+func max(num1, num2 int) int {
+  if (num1 >= num2) {
+    return num1
+  }
+
+  return num2
+}
+`;
+
+const post = {
+  title: `Min Max Game`,
+  tags: [`array`, `simulation`],
+  code: [goSolution],
+  date: `2025-05-28T00:00:00Z`,
+  difficulty: `easy`,
+  languages: [`Go`],
+  children: (
+    <p>
+      Fairly straight forward because the directions give you the parameters.
+      Basically you keep on trimming the array until you reach [num].
+    </p>
+  ),
+};
+
+export default post;
